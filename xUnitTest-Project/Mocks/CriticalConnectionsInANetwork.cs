2f94d63341ccca90d363cd5916727931876c1701
Kyle Chew <kyle.chew@hawaiianair.com>
2020-02-23 23:22:29 -0800

    examples

---
+++ xUnitTest-Project/Mocks/CriticalConnectionsInANetwork.cs
@@ -0,0 +1,123 @@
+﻿/*
+
+There are n servers numbered from 0 to n-1 connected by undirected server-to-server connections forming a network where connections[i] = [a, b] represents a connection between servers a and b. Any server can reach any other server directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some server unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+Output: [[1,3]]
+Explanation: [[3,1]] is also accepted.
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+n-1 <= connections.length <= 10^5
+connections[i][0] != connections[i][1]
+There are no repeated connections.
+
+https://www.geeksforgeeks.org/bridge-in-a-graph/
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using Xunit;
+
+namespace xUnitTest_Project.Mocks
+{
+    public partial class Solution
+    {
+        // Array of lists for Adjacency List Representation  
+        static List<int>[] adj;
+        int time = 0;
+
+        public static IList<PairInt> CriticalConnections(int V, IList<PairInt> connections)
+        {
+            // Mark all the vertices as not visited  
+            bool[] visited = new bool[V];
+            int[] disc = new int[V];
+            int[] low = new int[V];
+            int[] parent = new int[V];
+
+
+            // Initialize parent and visited,   
+            // and ap(articulation point) arrays  
+            for (int i = 0; i < V; i++)
+            {
+                parent[i] = -1;
+                visited[i] = false;
+            }
+
+            // Call the recursive helper function to find Bridges  
+            // in DFS tree rooted with vertex 'i'  
+            for (int i = 0; i < V; i++)
+                if (visited[i] == false)
+                    bridgeUtil(i, visited, disc, low, parent);
+
+        }
+
+        // A recursive function that finds and prints bridges  
+        // using DFS traversal  
+        // u --> The vertex to be visited next  
+        // visited[] --> keeps tract of visited vertices  
+        // disc[] --> Stores discovery times of visited vertices  
+        // parent[] --> Stores parent vertices in DFS tree  
+        private static void bridgeUtil(int u, bool[] visited, int[] disc,
+                        int[] low, int[] parent)
+        {
+
+            // Mark the current node as visited  
+            visited[u] = true;
+
+            // Initialize discovery time and low value  
+            disc[u] = low[u] = time++;
+
+            // Go through all vertices aadjacent to this  
+            foreach (int i in adj[u])
+            {
+                int v = i; // v is current adjacent of u  
+
+                // If v is not visited yet, then make it a child  
+                // of u in DFS tree and recur for it.  
+                // If v is not visited yet, then recur for it  
+                if (!visited[v])
+                {
+                    parent[v] = u;
+                    bridgeUtil(v, visited, disc, low, parent);
+
+                    // Check if the subtree rooted with v has a  
+                    // connection to one of the ancestors of u  
+                    low[u] = Math.Min(low[u], low[v]);
+
+                    // If the lowest vertex reachable from subtree  
+                    // under v is below u in DFS tree, then u-v is  
+                    // a bridge  
+                    if (low[v] > disc[u])
+                        Console.WriteLine(u + " " + v);
+                }
+
+                // Update low value of u for parent function calls.  
+                else if (v != parent[u])
+                    low[u] = Math.Min(low[u], disc[v]);
+            }
+        }
+    }
+
+    public class PairInt
+    {
+        public int First;
+        public int Second;
+    }
+}
---
+++ xUnitTest-Project/Mocks/CutOffTree.cs
@@ -0,0 +1,154 @@
+﻿/*
+
+You are asked to cut off trees in a forest for a golf event. The forest is represented as a non-negative 2D map, in this map:
+
+0 represents the obstacle can't be reached.
+1 represents the ground can be walked through.
+The place with number bigger than 1 represents a tree can be walked through, and this positive number represents the tree's height.
+ 
+
+You are asked to cut off all the trees in this forest in the order of tree's height - always cut off the tree with lowest height first. And after cutting, the original place has the tree will become a grass (value 1).
+
+You will start from the point (0, 0) and you should output the minimum steps you need to walk to cut off all the trees. If you can't cut off all the trees, output -1 in that situation.
+
+You are guaranteed that no two trees have the same height and there is at least one tree needs to be cut off.
+
+Example 1:
+
+Input: 
+[
+ [1,2,3],
+ [0,0,4],
+ [7,6,5]
+]
+Output: 6
+ 
+
+Example 2:
+
+Input: 
+[
+ [1,2,3],
+ [0,0,0],
+ [7,6,5]
+]
+Output: -1
+ 
+
+Example 3:
+
+Input: 
+[
+ [2,3,4],
+ [0,0,5],
+ [8,7,6]
+]
+Output: 6
+Explanation: You started from the point (0,0) and you can cut off the tree in (0,0) directly without walking.
+ 
+
+Hint: size of the given matrix will not exceed 50x50.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using Xunit;
+
+namespace xUnitTest_Project.Mocks
+{
+    public partial class Solution
+    {
+        public static int CutOffTree(IList<IList<int>> forest)
+        {
+            var f = forest.Select(x => x.ToArray()).ToArray();
+            if (f[0][0] == 0)
+            {
+                return -1;
+            }
+
+            List<(int x, int y, int t)> trees = new List<(int, int, int)>();
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < f[i].Length; j++)
+                {
+                    if (f[i][j] > 1)
+                    {
+                        trees.Add((i, j, f[i][j]));
+                    }
+                }
+            }
+
+            trees = trees.OrderBy(x => x.t).ToList();
+            int steps = 0;
+            int fromx = 0, fromy = 0;
+            f[fromx][fromy] = 1;
+
+            foreach (var tree in trees)
+            {
+                int currentStep = findMinSteps(f, (fromx, fromy), (tree.x, tree.y));
+                if (currentStep == -1)
+                {
+                    return -1;
+                }
+
+                fromx = tree.x;
+                fromy = tree.y;
+
+                // cut the tree
+                f[fromx][fromy] = 1;
+                steps += currentStep;
+            }
+
+            return steps;
+        }
+
+
+        private static int findMinSteps(int[][] f, (int x, int y) start, (int x, int y) end)
+        {
+
+            Queue<(int, int)> q = new Queue<(int, int)>();
+            int[,] directions = new int[,] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+            bool[,] visited = new bool[f.Length, f[0].Length];
+
+            q.Enqueue(start);
+            visited[start.x, start.y] = true;
+            int walks = 0;
+
+            while (q.Count > 0)
+            {
+                int count = q.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    (int x, int y) = q.Dequeue();
+                    if (x == end.x && y == end.y)
+                    {
+                        return walks;
+                    }
+
+                    int h = f[x][y];
+
+                    for (int d = 0; d < directions.GetLength(0); d++)
+                    {
+                        int newx = x + directions[d, 0];
+                        int newy = y + directions[d, 1];
+
+                        if (newx >= 0 && newy >= 0 && newx < f.Length && newy < f[0].Length
+                           && !visited[newx, newy]
+                           && f[newx][newy] != 0)
+                        {
+                            visited[newx, newy] = true;
+                            q.Enqueue((newx, newy));
+                        }
+                    }
+                }
+                walks++;
+            }
+
+            return -1;
+        }
+    }
+}
